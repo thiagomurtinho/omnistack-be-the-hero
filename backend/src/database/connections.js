174975d19bde31ea0f58b273c1b9b24bc0aad2cb
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-04-01 20:05:03 -0300

    Confgure env for tests on DB

--- backend/src/database/connections.js
+++ backend/src/database/connections.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configDB = require('../../knexfile');
 
-const connectionDB = knex(configDB.development);
+const connectionDB = knex(
+  process.env.NODE_ENV === 'test' ? configDB.test : configDB.development
+);
 
 module.exports = connectionDB;
