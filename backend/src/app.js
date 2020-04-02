7a75df7ef62d595b55171782e5330a1ffd96744a
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-04-02 01:28:51 -0300

    Configure errors of celebrate

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const cors = require('cors');
+const { errors } = require('celebrate');
 const express = require('express');
 const app = express();
 const routes = require('./routes');
@@ -6,5 +7,6 @@ const routes = require('./routes');
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 module.exports = app;
