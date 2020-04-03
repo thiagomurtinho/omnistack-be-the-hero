8296df7acfc012d22dfa1ff480000f2b9f06efc2
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-04-03 01:34:57 -0300

    Fiz require to routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const { errors } = require('celebrate');
 const express = require('express');
 const app = express();
-const routes = require('./routes');
+const routes = require('./routes/routes');
 
 app.use(cors());
 app.use(express.json());
