bd4b946933901511894a6d4e155c8c58d77b0d39
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-04-03 02:03:05 -0300

    Include ongs on validate file

--- backend/src/routes/Validate.js
+++ backend/src/routes/Validate.js
@@ -1,6 +1,15 @@
 const { Joi, Segments } = require('celebrate');
 
 module.exports = {
+  ongsPost: {
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.number().integer().required(), //Criar REGex
+      city: Joi.string().required(),
+      uf: Joi.string().length(2).required()
+    })
+  },
   incidentsPost: {
     [Segments.BODY]: Joi.object().keys({
       title: Joi.string().required(),
