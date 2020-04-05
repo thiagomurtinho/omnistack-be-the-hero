cefcef378e74dd0917c0cc0da1f13a775e6d6546
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-04-04 21:26:08 -0300

    Clean code fix " to '

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,9 +1,9 @@
-const connectionDb = require("../database/connections");
+const connectionDb = require('../database/connections');
 
 module.exports = {
   async index(req, res) {
-    const incidents = await connectionDb("incidents")
-      .select("*")
+    const incidents = await connectionDb('incidents')
+      .select('*')
       .catch(error => res.status(400).json({ err: error.toString() }));
 
     return res.status(200).json({ incidents });
@@ -12,7 +12,7 @@ module.exports = {
     const { title, description, value } = req.body;
     const ong_id = req.headers.auth;
 
-    const [id] = await connectionDb("incidents")
+    const [id] = await connectionDb('incidents')
       .insert({ title, description, value, ong_id })
       .catch(err => res.status(400).json({ err: err.toString() }));
 
